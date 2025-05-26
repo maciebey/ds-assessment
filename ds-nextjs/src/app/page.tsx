a844dd2b14fced8e41e7c182ae1e61ce68e09a4b
Macie Bey <maciebey@gmail.com>
2025-05-26 10:40:03 -0400

    UI - added datepicker dep, int select dates interface, query api by dates

--- ds-nextjs/src/app/page.tsx
+++ ds-nextjs/src/app/page.tsx
@@ -1,20 +1,81 @@
 'use client';
 import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+interface Row {
+  date: number;
+  Usage_kWh: number;
+  'Lagging_Current_Reactive.Power_kVarh': number;
+  Leading_Current_Reactive_Power_kVarh: number;
+  'CO2(tCO2)': number;
+  Lagging_Current_Power_Factor: number;
+  Leading_Current_Power_Factor: number;
+  NSM: number;
+  // TODO these could be enum
+  WeekStatus: string;
+  Day_of_week: string;
+  Load_Type: string;
+}
 
 export default function Home() {
-  const [data, setData] = useState('Loading');
-  useEffect(() => {
-    // fetch data from localhost:5000
-    fetch("http://127.0.0.1:5000")
+  const [data, setData] = useState<null | Row[]>();
+
+  // TODO: implement real timezone fix like: https://stackoverflow.com/a/61157388
+  const [startDate, setStartDate] = useState(new Date('2018-01-01T08:00:00Z'));
+  const [endDate, setEndDate] = useState(new Date('2018-01-03T08:00:00Z'));
+
+  const fetchRows = (start: Date, end: Date) => {
+    setData(null);
+    const url = `http://127.0.0.1:5000/?start_date=${start.toISOString()}&end_date=${end.toISOString()}`
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setData(JSON.stringify(data, null, 2));
+        setData(data as Row[]);
       })
+  }
+
+  useEffect(() => {
+    fetchRows(startDate, endDate);
   }, [])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {data}
+    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
+      <div className="flex">
+        Start Date:<DatePicker selected={startDate} onChange={(date) => date ? setStartDate(date) : null} />
+        End Date:<DatePicker selected={endDate} onChange={(date) => date ? setEndDate(date) : null} />
+        <button
+          className="ml-4 bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
+          onClick={() => {
+            fetchRows(startDate, endDate);
+          }
+          }
+        >
+          Fetch Data
+        </button>
+      </div>
+      <div className="flex">
+        <div>
+          {`Selected Start Date: ${startDate.toISOString()}       `}
+        </div>
+        <div className="ml-4">
+        {`Selected End Date: ${endDate.toISOString()}`}
+        </div>
+
+      </div>
+      <pre>
+      {data === null ? "Loading..." : (
+        <>
+          <div>{`Row Count: ${data!.length}`}</div>
+          {JSON.stringify(data, null, 2)}
+        </>
+      )}
+      
+
+      </pre>
     </div>
   );
 }
